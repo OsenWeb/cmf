6ee09c9b4245716d46917bc0e3c5ace53e7c5098
Andrii <andriybuts@gmail.com>
2014-11-14 15:02:31 +0200

    add swipes functionality to the carousel

--- web/js/jq-script.js
+++ web/js/jq-script.js
@@ -1,30 +1,84 @@
 myjQ(function($) {
 
-    $('.carousel .carousel-wrap').on('jcarousel:reload jcarousel:create', function () {
-        var carouselEl = $(this);
-        var width = carouselEl.innerWidth();
+    if($('.carousel').length) {
 
-        if (width >= 1660) {            
-            width = width/5;
-        } else if (width >= 1260) {          
-            width = width/4;
-        } else {            
-            width = width/3;
-        }
+        function scrollСarouselBack(carouselEl) {
+            carouselEl.jcarousel('scroll', '-=1');
+        };
 
-        carouselEl.jcarousel('items').css('width', width + 'px');
-    }).jcarousel({
-        wrap: 'circular'
-    });
+        function scrollСarouselForward(carouselEl) {
+            carouselEl.jcarousel('scroll', '+=1');
+        };
 
-    $('.carousel-back').on("click", function() {
-        var carouselEl = $(this).closest('.carousel').find('.carousel-wrap');
-        carouselEl.jcarousel('scroll', '-=1');
-    });
+        var xDown = null;
+        var yDown = null;
 
-    $('.carousel-forward').on("click", function() {
-        var carouselEl = $(this).closest('.carousel').find('.carousel-wrap');
-        carouselEl.jcarousel('scroll', '+=1');
-    });
+        function handleTouchStart(e) {
+            xDown = e.originalEvent.touches[0].clientX;
+            yDown = e.originalEvent.touches[0].clientY;
+        };
+
+        function handleTouchMove(e) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            };
+
+            var xUp = e.originalEvent.touches[0].clientX;
+            var yUp = e.originalEvent.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+                if ( xDiff > 0 ) {
+                    /* left swipe */
+                    var carouselEl = $(this);
+                    scrollСarouselForward(carouselEl);
+                } else {
+                    /* right swipe */
+                    var carouselEl = $(this);
+                    scrollСarouselBack(carouselEl);
+                };
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */ 
+                } else { 
+                    /* down swipe */
+                };
+            };
+            xDown = null;
+            yDown = null;
+        };
+
+        $('.carousel .carousel-wrap').on('jcarousel:reload jcarousel:create', function () {
+            var carouselEl = $(this);
+            var width = carouselEl.innerWidth();
+
+            if (width >= 1660) {
+                width = width/5;
+            } else if (width >= 1260) {
+                width = width/4;
+            } else {            
+                width = width/3;
+            }
+
+            carouselEl.jcarousel('items').css('width', width + 'px');
+        }).jcarousel({
+            wrap: 'circular'
+        });
+
+        $('.carousel-back').on("click", function() {
+            var carouselEl = $(this).closest('.carousel').find('.carousel-wrap');
+            scrollСarouselBack(carouselEl);
+        });
+
+        $('.carousel-forward').on("click", function() {
+            var carouselEl = $(this).closest('.carousel').find('.carousel-wrap');
+            scrollСarouselForward(carouselEl);
+        });
+
+        $('.carousel .carousel-wrap').on("touchstart", handleTouchStart);
+        $('.carousel .carousel-wrap').on("touchmove", handleTouchMove);
     
+    };
 });
